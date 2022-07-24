a4de49defe9f5ff2533181c3c3357239ecc40bbe
Ivan Demin <1ingego1@gmail.com>
2022-07-24 09:48:01 +0300

    init repo

---
+++ bin/game_bot_with_ai.dart
@@ -0,0 +1,5 @@
+import 'package:game_bot_with_ai/game_bot_with_ai.dart' as game_bot_with_ai;
+
+void main(List<String> arguments) {
+  print('Hello world: ${game_bot_with_ai.calculate()}!');
+}
---
+++ test/game_bot_with_ai_test.dart
@@ -0,0 +1,8 @@
+import 'package:game_bot_with_ai/game_bot_with_ai.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
